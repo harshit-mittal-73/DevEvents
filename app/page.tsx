657a031e0e29408833d4a9f22ca75a47e9473e8d
harshit-mittal-73 <harshitmittal0306@gmail.com>
2025-12-18 19:53:18 +0530

    base url correction

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,36 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import { IEvent } from "@/database";
-import { cacheLife } from "next/cache";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const page = async () => {
-  'use cache';
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
-  cacheLife('hours')
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const {events} = await response.json();
+            <ExploreBtn />
 
-  return (
-    <>
-    <section>
-      <h1 className="text-center">
-        The Hub For Every Dev <br/> Event You Can't Miss
-      </h1>
-      <p className="text-center mt-5">Hackathons, Meetups, Conferences, All in One Place</p>
-    </section>
-    <ExploreBtn/>
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
 
-    <div className="mt-20 space-y-7">
-      <h3>Featured Events</h3>
-
-      <ul className="events list-none">
-        {events && events.length > 0 && events.map((event : IEvent) => (
-            <li key={event.title}> <EventCard {...event} /> </li>
-        ))}
-      </ul>
-    </div>
-    </>
-  )
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
-export default page
+export default Page;
